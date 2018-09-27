f9a5abc1b387329b67a2af918b42a5d5b5170ea4
clock <18209263592@163.com>
2018-09-27 18:40:31 +0800

    init publish

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# fangtian
+# fangtian(方天画戟)
+
+1、给 dist/components/{COMPONENTNAME}/styles/css.js 添加以下内容：
+
+```javascript
+import './index.css'
+import '../../styles/index.css'
+```
---
+++ huaji/index.js
@@ -0,0 +1,66 @@
+const glob = require('glob')
+const fs = require('fs')
+const shelljs = require('shelljs')
+const chalk = require('chalk')
+var babel = require('@babel/core')
+
+function FangTianHuaJi() {
+  this.cssJs = `import './index.css'\nimport '../../styles/index.css'`
+}
+
+FangTianHuaJi.prototype.excute = function() {
+  glob.sync('src/components/*/style/*.tsx').forEach(path => {
+    const cssOutputDir = `${path
+      .replace(/^src/, 'dist')
+      .replace(/components/, 'es')
+      .split('/')
+      .splice(0, 4)
+      .join('/')}`
+
+    const cssOutputPath = `${cssOutputDir}/css.js`
+
+    shelljs.mkdir('-p', cssOutputDir)
+    shelljs.touch(`${cssOutputPath}`)
+
+    fs.writeFileSync(cssOutputPath, this.cssJs, err => {
+      err && console.log(chalk.red(err))
+    })
+  })
+}
+
+FangTianHuaJi.prototype.tail = function() {
+  glob.sync('src/components/**/index.tsx').forEach(path => {
+    const reg = new RegExp(/style/)
+    const isStyle = reg.test(path.split('/')[3]) || reg.test(path.split('/')[2])
+
+    if (!isStyle) return
+
+    fs.readFile(path, (err, data) => {
+      const outputPath = path
+        .replace(/^src/, 'dist')
+        .replace(/components/, 'es')
+        .replace(/.tsx$/, '.js')
+      err && console.log(chalk.red(err))
+      fs.writeFileSync(outputPath, data)
+    })
+
+    // lib version
+    babel.transformFile(path, {}, function(err, result) {
+      const outputPath = path
+        .replace(/^src/, 'dist')
+        .replace(/components/, 'lib')
+        .replace(/.tsx$/, '.js')
+      err && console.log(chalk.red(err))
+      fs.writeFileSync(outputPath, result.code)
+    })
+  })
+}
+
+FangTianHuaJi.prototype.clear = function() {
+  shelljs.rm('-rf', 'dist')
+}
+
+const fangTianHuaJi = new FangTianHuaJi()
+module.exports = {
+  FangTianHuaJi
+}
